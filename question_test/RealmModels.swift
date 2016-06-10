97478aaa454c7756def93b6926c8a00f665f4a42
XXXXX <XXXXX@Xmail.commkdir>
2016-06-10 11:30:12 +0900

    ・RealmのFrameworks追加(Embed) ・CSVファイル→RealmBrowserでdefault.realmを作成 ・default.realmの[Q,A1,A2,A3,A4,A5]をデバッグエリアに表示させた

---
+++ question_test/RealmModels.swift
@@ -0,0 +1,12 @@
+import Foundation
+import RealmSwift
+
+class realm_test: Object {
+  dynamic var Q = ""
+  dynamic var A1 = ""
+  dynamic var A2 = ""
+  dynamic var A3 = ""
+  dynamic var A4 = ""
+  dynamic var A5 = ""
+}
+
--- question_test/ViewController.swift
+++ question_test/ViewController.swift
@@ -2,22 +2,45 @@
 //  ViewController.swift
 //  question_test
 //
-//  Created by 横内翔平 on 2016/06/09.
+//  Created by user on 2016/06/09.
 //
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
+    
+    private var Questions:[String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        getAllQuestions()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func getAllQuestions() {
+        let config = Realm.Configuration(
+            fileURL: NSBundle.mainBundle().URLForResource("default", withExtension: "realm"),
+            readOnly: true)
+        
+        let realm = try! Realm(configuration: config)
+        
+        let results = realm.objects(realm_test).filter("Q != ''")
+        
+        for i in 0 ..< results.count {
+            Questions.append(results[i].Q)
+        }
+        
+        print("getAllQuestions=\(results)")
     }
 
 
